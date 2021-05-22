36be885c7844bdaa09cb6cf12b68cb7ba8e12b9c
Fariz <farizalfairuz27@gmail.com>
2021-05-22 09:01:15 +0700

    delete: comment

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,62 +4,11 @@ import { Link } from 'react-router-dom'
 
 
 function SearchPage() {
-    // let history = useHistory();
-    // const [nama, setNama] = useState(null)
     const [search, setSearch] = useState(null)
-    // const [kategori, setKategori] = useState(null)
-    // const [nIlmiah, setNIlmiah] = useState(null)
-    // const [query, setQuery] = useState({
-    //     name: "",
-    //     category: "",
-    //     nama_ilmiah: "",
-    // })
-
 
     const inputHandler = (e) => {
         setSearch(e.target.value)
     }
-
-    // const onSubmitHandler = (e) => {
-    //     e.preventDefault();
-    //     setQuery({
-    //         name: nama,
-    //         category: kategori,
-    //         nama_ilmiah: nIlmiah
-    //     })
-        
-
-        // axios
-        //     .get(`http://localhost:5000/api/cariman/?nama=${query.nama}`)
-        //     .then((response) => {
-        //         console.log(response.data)
-        //     })
-        //     .catch((err) => {
-        //         console.log(err)
-        //     })
-
-        // axios
-        //     .get(`http://localhost:5000/api/cariman/?kategori=${query.kategori}`)
-        //     .then((response) => {
-        //         console.log(response.data)
-        //     })
-        //     .catch((err) => {
-        //         console.log(err)
-        //     })
-
-        // axios
-        //     .get(`http://localhost:5000/api/cariman/?nama_ilmiah=${query.nama_ilmiah}`)
-        //     .then((response) => {
-        //         console.log(response.data)
-        //     })
-        //     .catch((err) => {
-        //         console.log(err)
-        //     })
-
-        // history.push("/result")
-    // }
-
-    
 
     return (
         <div id="search-page">
